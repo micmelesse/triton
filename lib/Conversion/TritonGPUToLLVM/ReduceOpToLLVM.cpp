3bc27eea622ed05536b6eba7df5d4cc50247ed1f
Michael Melesse <micmelesse@gmail.com>
2024-02-15 15:20:41 -0600

    save

--- lib/Conversion/TritonGPUToLLVM/ReduceOpToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/ReduceOpToLLVM.cpp
@@ -90,7 +90,7 @@ private:
     }
 
     // Create a new copy of the reduce block, and inline it
-    Block *currentBlock = rewriter.getBlock();
+    Block *currentBlock = rewriter.getBlock(); // try
     Region &parent = *currentBlock->getParent();
     rewriter.cloneRegionBefore(combineOp, &parent.front());
     auto &newReduce = parent.front();
